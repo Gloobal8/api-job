01e4d8ee5fed8faf61f99736c5e20aa60265560e
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2025-07-22 13:22:12 -0400

    feat: save develop

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -48,21 +48,12 @@
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
 
-<<<<<<< HEAD
           <v-list-item v-if="isLoggedIn" to="/dashboard" link>
             <template v-slot:prepend>
               <v-icon>mdi-view-dashboard-outline</v-icon>
             </template>
             <v-list-item-title>Dashboard</v-list-item-title>
           </v-list-item>
-=======
-        <v-list-item v-if="isLoggedIn" to="/dashboard" link>
-          <template v-slot:prepend>
-            <v-icon>mdi-view-dashboard-outline</v-icon>
-          </template>
-          <v-list-item-title>Dashboard</v-list-item-title>
-        </v-list-item>
->>>>>>> develop
 
           <v-list-item to="/jobs" link>
             <template v-slot:prepend>
@@ -77,8 +68,6 @@
             </template>
             <v-list-item-title>Companies</v-list-item-title>
           </v-list-item>
-
-<<<<<<< HEAD
           <v-list-item v-if="isLoggedIn" to="/profile" link>
             <template v-slot:prepend>
               <v-icon>mdi-account</v-icon>
@@ -92,7 +81,6 @@
             </template>
             <v-list-item-title>Messages</v-list-item-title>
           </v-list-item>
-=======
         <v-list-item v-if="isLoggedIn" to="/profile" link>
           <template v-slot:prepend>
             <v-icon>mdi-account</v-icon>
@@ -106,7 +94,6 @@
           </template>
           <v-list-item-title>Messages</v-list-item-title>
         </v-list-item>
->>>>>>> develop
 
           <!--<v-list-item to="/notifications" link>
           <template v-slot:prepend>
